67d59bb1727330e08ef2767fb350267559dcd44f
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-27 17:40:18 -0300

    building initial app ui

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:movingPictures/presentation/core/constants.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      title: appName,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
