4f6a76dbeb9510725b93f3757b1074626f070c37
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-27 17:28:38 -0300

    building initial app ui

---
+++ lib/presentation/core/app_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
