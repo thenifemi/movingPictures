73d00241f817fddaff6172ace8396022a2cfec60
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-28 12:30:43 -0300

    added fonts

--- lib/presentation/core/theme.dart
+++ lib/presentation/core/theme.dart
@@ -5,29 +5,43 @@ import 'app_colors.dart';
 ThemeData appTheme() {
   TextTheme _appTextTheme(TextTheme base) {
     return base.copyWith(
+      //*[headline5] is for all kinds of headings EXCEPT Movie Title.
       headline5: base.headline5.copyWith(
         fontFamily: 'Montserrat',
-        fontSize: 30.0,
+        fontWeight: FontWeight.w500,
+        fontSize: 22.0,
         color: AppColors.white,
       ),
+
+      //*[headline6] is for Movie and series titles, even actor titles when necessary
       headline6: base.headline6.copyWith(
-        fontFamily: 'Roboto',
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.w500,
+        fontSize: 20.0,
+        color: AppColors.white,
+      ),
+
+      //*[bodyText1] is for movie info and likes. Alsp for regular app texts.
+      bodyText1: base.bodyText1.copyWith(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.w400,
         fontSize: 15.0,
         color: AppColors.white,
       ),
-      headline4: base.headline4.copyWith(
-        fontFamily: 'Roboto',
-        fontSize: 24.0,
-        color: AppColors.white,
-      ),
-      headline3: base.headline3.copyWith(
-        fontFamily: 'Roboto',
-        fontSize: 18.0,
-        color: AppColors.white,
-      ),
-      bodyText1: base.bodyText1.copyWith(
-        fontFamily: 'Roboto',
+
+      //*[subtitle1] is for small deatils like movie duration, movie crew list and the likes
+      subtitle1: base.subtitle1.copyWith(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.w300,
         fontSize: 14.0,
+        color: AppColors.white,
+      ),
+
+      //*[Button] Obviously is for button text.
+      button: base.button.copyWith(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.w500,
+        fontSize: 16.0,
         color: AppColors.white,
       ),
     );
--- lib/presentation/splash/splash_screen.dart
+++ lib/presentation/splash/splash_screen.dart
@@ -1,25 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Heading",
-              style: Theme.of(context).textTheme.headline5,
-            ),
-            Text("title"),
-            Text("body"),
-            Text("subtext"),
-            Text("regular text"),
-            Text("button text"),
-          ],
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light,
+      ),
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Heading",
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              Text(
+                "Title",
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Text(
+                "body",
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              Text(
+                "subtext",
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              Text(
+                "regular text",
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              Text(
+                "Button text",
+                style: Theme.of(context).textTheme.button,
+              ),
+            ],
+          ),
         ),
       ),
     );
