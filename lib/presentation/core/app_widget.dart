9f95ee8f07cdcbbf602981fa67cf835cca356ef3
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-28 11:42:37 -0300

    added theming

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:movingPictures/presentation/core/theme.dart';
 
 import '../routes/router.gr.dart';
 import 'constants.dart';
@@ -10,6 +11,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: appName,
       debugShowCheckedModeBanner: false,
+      theme: appTheme(),
       builder: ExtendedNavigator.builder(
         router: AppRouter(),
       ),
--- lib/presentation/core/theme.dart
+++ lib/presentation/core/theme.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
 
+import 'app_colors.dart';
+
 ThemeData appTheme() {
   TextTheme _appTextTheme(TextTheme base) {
-    return base.copyWith(headline5: base.headline5.copyWith(
-      fontFamily: ,
-      fontSize: 22.0,
-      color: Colors.black, 
-    ));
+    return base.copyWith(
+      headline6: base.headline6.copyWith(
+        fontFamily: 'Roboto',
+        fontSize: 15.0,
+        color: AppColors.white,
+      ),
+      headline5: base.headline5.copyWith(
+        fontFamily: 'Roboto',
+        fontSize: 22.0,
+        color: AppColors.white,
+      ),
+      headline4: base.headline4.copyWith(
+        fontFamily: 'Roboto',
+        fontSize: 24.0,
+        color: AppColors.white,
+      ),
+      headline3: base.headline3.copyWith(
+        fontFamily: 'Roboto',
+        fontSize: 18.0,
+        color: AppColors.white,
+      ),
+      bodyText1: base.bodyText1.copyWith(
+        fontFamily: 'Roboto',
+        fontSize: 14.0,
+        color: AppColors.white,
+      ),
+    );
   }
+
+  final ThemeData base = ThemeData();
+  return base.copyWith(
+    textTheme: _appTextTheme(base.textTheme),
+    iconTheme: IconThemeData(
+      color: AppColors.gray,
+      size: 20.0,
+    ),
+    primaryColor: AppColors.black,
+    scaffoldBackgroundColor: AppColors.black,
+    buttonColor: AppColors.red,
+    backgroundColor: AppColors.black,
+  );
 }
