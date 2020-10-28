f871a0dcdb1c347ed987ebd7d2fbb40cc473b5dc
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-28 11:21:27 -0300

    added theming

---
+++ lib/presentation/core/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData appTheme() {
+  TextTheme _appTextTheme(TextTheme base) {
+    return base.copyWith(headline5: base.headline5.copyWith(
+      fontFamily: ,
+      fontSize: 22.0,
+      color: Colors.black, 
+    ));
+  }
+}
---
+++ lib/presentation/routes/router.gr.dart
@@ -0,0 +1,45 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+
+// ignore_for_file: public_member_api_docs
+
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/cupertino.dart';
+
+import '../splash/splash_screen.dart';
+
+class Routes {
+  static const String splashScreen = '/';
+  static const all = <String>{
+    splashScreen,
+  };
+}
+
+class AppRouter extends RouterBase {
+  @override
+  List<RouteDef> get routes => _routes;
+  final _routes = <RouteDef>[
+    RouteDef(Routes.splashScreen, page: SplashScreen),
+  ];
+  @override
+  Map<Type, AutoRouteFactory> get pagesMap => _pagesMap;
+  final _pagesMap = <Type, AutoRouteFactory>{
+    SplashScreen: (data) {
+      return CupertinoPageRoute<dynamic>(
+        builder: (context) => const SplashScreen(),
+        settings: data,
+      );
+    },
+  };
+}
+
+/// ************************************************************************
+/// Navigation helper methods extension
+/// *************************************************************************
+
+extension AppRouterExtendedNavigatorStateX on ExtendedNavigatorState {
+  Future<dynamic> pushSplashScreen() => push<dynamic>(Routes.splashScreen);
+}
