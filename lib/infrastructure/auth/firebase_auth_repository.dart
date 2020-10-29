0ede6299879c789b7b3b5622cf7d9ee7826f98ba
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-29 12:28:31 -0300

    building auth infrastructure

---
+++ lib/infrastructure/auth/firebase_auth_repository.dart
@@ -0,0 +1,52 @@
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import '../../domain/auth/app_user.dart';
+import '../../domain/auth/auth_failure.dart';
+import '../../domain/auth/auth_repository_interface.dart';
+import 'firebase_user_mapper.dart';
+
+class FirebaseAuthRepository implements AuthRepositoryInterface {
+  final FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
+
+  FirebaseAuthRepository(
+    this._firebaseAuth,
+    this._googleSignIn,
+  );
+
+  @override
+  Future<Either<AuthFailure, Unit>> signInWithGoogle() async {
+    try {
+      final googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        return left(const AuthFailure.cancelledByUser());
+      }
+
+      final googleAuthentication = await googleUser.authentication;
+
+      final authCredential = GoogleAuthProvider.credential(
+        idToken: googleAuthentication.idToken,
+        accessToken: googleAuthentication.accessToken,
+      );
+
+      //returning [unit] because the transaction was successsful with no errors.
+      return _firebaseAuth
+          .signInWithCredential(authCredential)
+          .then((r) => right(unit));
+    } on FirebaseAuthException catch (_) {
+      return left(const AuthFailure.serverError());
+    }
+  }
+
+  @override
+  Future<Option<AppUser>> getSignedInUser() async =>
+      optionOf(_firebaseAuth.currentUser?.toDomain());
+
+  @override
+  Future<void> signOut() => Future.wait([
+        _googleSignIn.signOut(),
+        _firebaseAuth.signOut(),
+      ]);
+}
---
+++ lib/infrastructure/auth/firebase_user_mapper.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart' as firebase;
+
+import '../../domain/auth/app_user.dart';
+import '../../domain/core/value_objects.dart';
+
+extension FirebaseUserDomainX on firebase.User {
+  AppUser toDomain() {
+    return AppUser(id: UniqueId.fromUniqueString(uid));
+  }
+}
