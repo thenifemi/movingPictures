27c05fd6fc753cfa450bd89d61486a43d8e5c5fa
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-31 12:04:34 -0300

    added some core widgets

---
+++ lib/presentation/core/saving_in_progress_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SavingInProgressOverlay extends StatelessWidget {
+  final bool isSaving;
+  const SavingInProgressOverlay({
+    Key key,
+    @required this.isSaving,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IgnorePointer(
+      ignoring: !isSaving,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 150),
+        color: isSaving ? Colors.black.withOpacity(0.6) : Colors.transparent,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Visibility(
+          visible: isSaving,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              CircularProgressIndicator(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
