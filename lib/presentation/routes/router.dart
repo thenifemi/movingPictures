b8aed969540b3858692093ce2102ac8a7d6fc5e5
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-11-06 11:39:54 -0300

    added bottom nav bar

--- lib/presentation/routes/router.dart
+++ lib/presentation/routes/router.dart
@@ -1,7 +1,7 @@
 import 'package:auto_route/auto_route_annotations.dart';
-import 'package:movingPictures/presentation/main_layout_appbar_navbar/main_body_layout.dart';
-import 'package:movingPictures/presentation/signin/sign_in_screen.dart';
 
+import '../main_layout_appbar_navbar/main_body_layout.dart';
+import '../signin/sign_in_screen.dart';
 import '../splash/splash_screen.dart';
 
 @MaterialAutoRouter(
