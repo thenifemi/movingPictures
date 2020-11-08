d882d6a12598ba3984ca51bee1804dac95301ff3
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-11-07 23:30:53 -0300

    ..

--- lib/presentation/profile/profile_screen.dart
+++ lib/presentation/profile/profile_screen.dart
@@ -1,12 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movingPictures/presentation/core/constants/language_constants.dart';
 
 import '../../application/auth/auth_bloc.dart';
 import '../../domain/auth/app_user.dart';
 import '../core/app_colors.dart';
 import '../core/component_widgets/cancel_button_widget.dart';
+import '../core/constants/language_constants.dart';
 
 class ProfileScreen extends StatelessWidget {
   final TextTheme appTextTheme;
