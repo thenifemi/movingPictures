64c00bc219fdee9670874092beb795a3a977d8c7
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-11-07 23:17:00 -0300

    ..

---
+++ lib/presentation/friends/friends.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Friends extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+}
