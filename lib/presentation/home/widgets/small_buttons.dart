1d1a9439449b39e15dbb5dd500608fc69b4e17aa
Oluwatimilehin Diffu <thenifemi@gmail.com>
2020-11-10 12:31:59 -0300

    banners

---
+++ lib/presentation/home/widgets/small_buttons.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../core/app_colors.dart';
+import '../../core/constants/constants.dart';
+import '../../core/constants/language_constants.dart';
+
+class ShareButtonWidget extends StatelessWidget {
+  final TextTheme appTextTheme;
+  final Function onPressed;
+
+  const ShareButtonWidget({
+    Key key,
+    @required this.appTextTheme,
+    @required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Column(
+        children: [
+          SvgPicture.asset(
+            shareIcon,
+            color: AppColors.white,
+          ),
+          const SizedBox(height: 5.0),
+          Text(
+            share,
+            style: appTextTheme.subtitle1,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FavoriteButtonWidget extends StatelessWidget {
+  final TextTheme appTextTheme;
+  final Function onPressed;
+  const FavoriteButtonWidget({
+    Key key,
+    @required this.appTextTheme,
+    @required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Column(
+        children: [
+          SvgPicture.asset(
+            favoriteIcon,
+            color: AppColors.white,
+          ),
+          const SizedBox(height: 5.0),
+          Text(
+            favorite,
+            style: appTextTheme.subtitle1,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class InfoButtonWidget extends StatelessWidget {
+  final TextTheme appTextTheme;
+  final Function onPressed;
+  const InfoButtonWidget({
+    Key key,
+    @required this.appTextTheme,
+    @required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Column(
+        children: [
+          SvgPicture.asset(
+            infoIcon,
+            color: AppColors.white,
+          ),
+          const SizedBox(height: 5.0),
+          Text(
+            info,
+            style: appTextTheme.subtitle1,
+          ),
+        ],
+      ),
+    );
+  }
+}
