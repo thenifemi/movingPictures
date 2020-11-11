b19f318400974f71ccdb15d7392d6377b2ef6e11
Oluwatimilehin Diffu <thenifemi@gmail.com>
2020-11-11 12:47:29 -0300

    ..

--- lib/presentation/movie_or_series_info/movie_or_series_info.dart
+++ lib/presentation/movie_or_series_info/movie_or_series_info.dart
@@ -1,9 +1,7 @@
-import 'dart:ui';
-
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:movingPictures/presentation/core/app_colors.dart';
-import 'package:movingPictures/presentation/core/constants/constants.dart';
+
+import '../core/app_colors.dart';
+import 'widgets/information_items_widget.dart';
 
 class MovieOrSeriesInfo extends StatelessWidget {
   @override
@@ -11,40 +9,10 @@ class MovieOrSeriesInfo extends StatelessWidget {
     final appTextTheme = Theme.of(context).textTheme;
     return Scaffold(
       body: NestedScrollView(
+          physics: const BouncingScrollPhysics(),
           headerSliverBuilder: (context, innerBoxIsScrolled) {
             return [
-              SliverAppBar(
-                leading: IconButton(
-                  icon: const Icon(Icons.arrow_back_ios),
-                  onPressed: () => ExtendedNavigator.of(context).pop(),
-                ),
-                expandedHeight: MediaQuery.of(context).size.height / 1.8,
-                pinned: true,
-                flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  background: Container(
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: ExactAssetImage(theQueensGambitPoster),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-                      child: Container(
-                        decoration:
-                            BoxDecoration(color: Colors.white.withOpacity(0.0)),
-                        child: Center(
-                          child: Text(
-                            "DOJA",
-                            style: appTextTheme.headline5,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+              InformationItemsWidget(appTextTheme: appTextTheme),
             ];
           },
           body: Container(
