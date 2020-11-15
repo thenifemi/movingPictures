36a8ec912a922795fa42267f1ebe852f70c0b953
Oluwatimilehin Diffu <thenifemi@gmail.com>
2020-11-14 22:14:07 -0300

    intl

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,10 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import '../../application/auth/auth_bloc.dart';
 import '../../injection.dart';
 import '../routes/router.gr.dart';
+import 'app_localizations.dart';
 import 'constants/constants.dart';
 import 'theme.dart';
 
@@ -25,6 +27,28 @@ class App extends StatelessWidget {
         builder: ExtendedNavigator.builder(
           router: AppRouter(),
         ),
+        supportedLocales: const [
+          Locale('en', 'US'),
+          Locale('pt', 'BR'),
+        ],
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        localeResolutionCallback: (locale, supportedLocales) {
+          //* Check if the device current locale is supported
+          for (final supportedLocale in supportedLocales) {
+            if (supportedLocale.languageCode == locale.languageCode &&
+                supportedLocale.countryCode == locale.countryCode) {
+              return supportedLocale;
+            }
+          }
+          //* If the device current local isn't supported, use the first one
+          //* from the list
+          return supportedLocales.first;
+        },
       ),
     );
   }
