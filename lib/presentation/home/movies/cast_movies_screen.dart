63338ec1327166bfb5e10cc90a2a15c983e2789e
Oluwatimilehin Diffu <thenifemi@gmail.com>
2020-11-30 11:58:57 -0300

    ..

---
+++ lib/presentation/home/movies/cast_movies_screen.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../application/home/movies/movies/movies_bloc.dart';
+import '../../../domain/home/movies/cast/cast.dart';
+import '../../../domain/home/movies/movie_sub/movie_sub.dart';
+import '../../../infrastructure/core/credentials.dart';
+import '../../../injection.dart';
+import '../../core/app_colors.dart';
+import '../../core/component_widgets/cancel_button_widget.dart';
+import '../../core/component_widgets/movie_loading_wigdet.dart';
+import '../widgets/build_show_info_modal_bottom_sheet_widget.dart';
+
+class CastMoviesScreen extends StatelessWidget {
+  final Cast cast;
+
+  const CastMoviesScreen({
+    Key key,
+    @required this.cast,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final appTextTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: const CancelButton(),
+        title: Text(cast.name, style: appTextTheme.headline5),
+      ),
+      body: BlocProvider(
+        create: (context) =>
+            getIt<MoviesBloc>()..add(MoviesEvent.movieByCastIdCalled(cast.id)),
+        child: BlocBuilder<MoviesBloc, MoviesState>(
+          builder: (context, state) {
+            return state.map(
+              initial: (_) => const MovieLoadingWidget(),
+              loading: (_) => const MovieLoadingWidget(),
+              loadSuccess: (state) {
+                final movies = state.movies;
+
+                return Movies(
+                  movies: movies,
+                  appTextTheme: appTextTheme,
+                );
+              },
+              loadFailure: (_) => Container(
+                height: 100.0,
+                color: AppColors.red,
+              ),
+              loadSuccessforMovie: (_) => null,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Movies extends StatelessWidget {
+  const Movies({
+    Key key,
+    @required this.movies,
+    @required this.appTextTheme,
+  }) : super(key: key);
+
+  final List<MovieSub> movies;
+  final TextTheme appTextTheme;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    /*24 is for notification bar on Android*/
+    final double itemHeight = (size.height - kToolbarHeight - 24) / 2.5;
+    final double itemWidth = size.width / 2;
+
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: GridView.count(
+        childAspectRatio: itemWidth / itemHeight,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0,
+        physics: const BouncingScrollPhysics(),
+        crossAxisCount: 3,
+        children: List.generate(
+          movies.length,
+          (i) {
+            final movie = movies[i];
+            return GestureDetector(
+              onTap: () => buildShowInfoModalBottomSheet(
+                appTextTheme: appTextTheme,
+                context: context,
+                movieId: movie.id,
+              ),
+              child: Tooltip(
+                message: movie.title,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(5.0),
+                  child: Image.network(
+                    "$MOVIE_POSTER_PATH${movie.poster_path}",
+                    fit: BoxFit.fill,
+                    loadingBuilder: (context, child, loadingProgress) {
+                      if (loadingProgress == null) return child;
+                      return Center(
+                        child: CircularProgressIndicator(
+                          value: loadingProgress.expectedTotalBytes != null
+                              ? loadingProgress.cumulativeBytesLoaded /
+                                  loadingProgress.expectedTotalBytes
+                              : null,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/movie_or_series_info/widgets/build_cast_modal_sheet.dart
@@ -0,0 +1,86 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+import '../../../domain/home/movies/cast/cast.dart';
+import '../../../domain/home/movies/movie/movie.dart';
+import '../../core/app_colors.dart';
+import '../../core/component_widgets/cancel_button_widget.dart';
+import '../../routes/router.gr.dart';
+
+Future buildCastModalSheet({
+  @required BuildContext context,
+  @required List<Cast> casts,
+  @required Movie movie,
+}) {
+  return showModalBottomSheet(
+    isScrollControlled: true,
+    backgroundColor: AppColors.gray,
+    shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+      topLeft: Radius.circular(15.0),
+      topRight: Radius.circular(15.0),
+    )),
+    context: context,
+    builder: (context) {
+      final appTextTheme = Theme.of(context).textTheme;
+
+      return Container(
+        height: MediaQuery.of(context).size.height / 1.05,
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Align(
+                alignment: Alignment.topRight,
+                child: CancelButton(),
+              ),
+              Text(
+                movie.title,
+                style: const TextStyle(
+                  color: AppColors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              const Text(
+                "Cast",
+                style: TextStyle(
+                  color: AppColors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: casts.length,
+                  itemBuilder: (context, i) {
+                    final cast = casts[i];
+                    return Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Center(
+                        child: GestureDetector(
+                          onTap: () => ExtendedNavigator.of(context)
+                              .pushCastMoviesScreen(cast: cast),
+                          child: Text(
+                            cast.name,
+                            style: TextStyle(
+                              color: appTextTheme.subtitle1.color,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
