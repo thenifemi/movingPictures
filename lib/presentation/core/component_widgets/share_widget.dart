18f1ea3d9e55f85328e7150e6c0ac080a03d42a4
Oluwatimilehin Diffu <thenifemi@gmail.com>
2020-12-01 13:56:33 -0300

    ..

---
+++ lib/presentation/core/component_widgets/share_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/foundation.dart';
+import 'package:movingPictures/domain/home/movies/movie/movie.dart';
+// import 'package:share/share.dart';
+
+// void shareWidget({
+//   @required Movie movie,
+// }) {
+//   Share.shareFiles(paths)
+// }
