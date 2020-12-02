bc8fc05e8d275bef345b2e655a9154a8a404614e
Oluwatimilehin Diffu <thenifemi@gmail.com>
2020-12-02 12:20:20 -0300

    building favorite movies

---
+++ lib/domain/home/movies/favorite_movies_interface.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+
+import 'movie/movies_failure.dart';
+import 'movie_sub/movie_sub.dart';
+
+abstract class FavoritesMoviesInterface {
+  Stream<Either<MovieFailure, List<MovieSub>>> watchMovieFavorites();
+  Future<Either<MovieFailure, Unit>> createFavoriteMovie();
+  Future<Either<MovieFailure, Unit>> deleteFavoriteMovie();
+}
