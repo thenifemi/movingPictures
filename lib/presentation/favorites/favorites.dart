f2c931e5edfc6ddea583c6f8d82058eea3385b06
Oluwatimilehin Diffu <thenifemi@gmail.com>
2020-12-05 18:16:14 -0300

    ..

--- lib/presentation/favorites/favorites.dart
+++ lib/presentation/favorites/favorites.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:movingPictures/presentation/core/app_colors.dart';
+import 'package:movingPictures/presentation/favorites/favorite_series_tab.dart';
 
 import '../core/app_localizations.dart';
 import '../core/component_widgets/cancel_button_widget.dart';
@@ -14,6 +16,7 @@ class Favorites extends StatelessWidget {
     @required this.automaticallyImplyLeading,
     this.tabController,
   }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final lang = AppLocalizations.of(context);
@@ -24,8 +27,30 @@ class Favorites extends StatelessWidget {
         automaticallyImplyLeading: automaticallyImplyLeading,
         leading: automaticallyImplyLeading ? const CancelButton() : null,
         title: Text(lang.translate(favorites), style: appTextTheme.headline5),
+        bottom: TabBar(
+          controller: tabController,
+          isScrollable: true,
+          indicatorColor: AppColors.red,
+          indicatorSize: TabBarIndicatorSize.label,
+          indicatorWeight: 3.0,
+          labelColor: appTextTheme.bodyText1.color,
+          unselectedLabelColor: AppColors.white,
+          tabs: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: Text(lang.translate(movies).toUpperCase()),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: Text(lang.translate(series).toUpperCase()),
+            ),
+          ],
+        ),
       ),
-      body: const FavoriteMoviesTab(),
+      body: TabBarView(
+        controller: tabController,
+        children: const [FavoriteMoviesTab(), FavoriteSeriesTab()],
+      ),
     );
   }
 }
