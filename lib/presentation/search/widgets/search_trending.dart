18a33dd1c2d8bbcb1b75ffe23a1bd3d27d8ea88a
Oluwatimilehin Diffu <thenifemi@gmail.com>
2020-12-10 11:42:46 -0300

    search trending

---
+++ lib/presentation/search/widgets/search_trending.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../application/search/search_bloc.dart';
+import '../../../domain/search/search.dart';
+import '../../../injection.dart';
+import '../../core/component_widgets/movie_loading_wigdet.dart';
+import 'trending_movies.dart';
+import 'trending_series.dart';
+
+class SearchTrending extends StatelessWidget {
+  const SearchTrending({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) =>
+          getIt<SearchBloc>()..add(const SearchEvent.trendingCalled()),
+      child: BlocBuilder<SearchBloc, SearchState>(
+        builder: (context, state) {
+          return state.map(
+            initial: (_) => const MovieLoadingWidget(),
+            loading: (_) => const MovieLoadingWidget(),
+            loadSuccess: (state) =>
+                Trending(moviesOrSeries: state.moviesOrSeries),
+            loadFailure: (_) => const MovieErrorWidget(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class Trending extends StatelessWidget {
+  const Trending({
+    Key key,
+    @required this.moviesOrSeries,
+  }) : super(key: key);
+
+  final List<Search> moviesOrSeries;
+
+  @override
+  Widget build(BuildContext context) {
+    // final lang = AppLocalizations.of(context);
+    final appTextTheme = Theme.of(context).textTheme;
+
+    final size = MediaQuery.of(context).size;
+    /*24 is for notification bar on Android*/
+    final double itemHeight = (size.height - kToolbarHeight - 24) / 2.5;
+    final double itemWidth = size.width / 2;
+
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.topLeft,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Top Trending",
+              style: appTextTheme.headline5,
+            ),
+          ),
+        ),
+        Expanded(
+          child: GridView.count(
+            shrinkWrap: true,
+            childAspectRatio: itemWidth / itemHeight,
+            crossAxisSpacing: 10.0,
+            mainAxisSpacing: 10.0,
+            crossAxisCount: 3,
+            children: List.generate(
+              moviesOrSeries.length,
+              (i) {
+                final movieOrSerie = moviesOrSeries[i];
+                return movieOrSerie.mediaType == "movie"
+                    ? TrendingMovies(movieOrSerie: movieOrSerie)
+                    : TrendingSeries(movieOrSerie: movieOrSerie);
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
