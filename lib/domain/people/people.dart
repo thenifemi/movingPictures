21e1e60e82fb11502cc650ce4bc2e46ad6796de9
Oluwatimilehin Diffu <thenifemi@gmail.com>
2020-12-12 14:45:38 -0300

    ..

--- lib/domain/people/people.dart
+++ lib/domain/people/people.dart
@@ -1,15 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
 
-import '../home/movies/favorite_movies/favorite_movies.dart';
-import '../home/series/favorite_serie/favorite_serie.dart';
-
 class Person {
   final String email;
   final String name;
   final String photoURL;
-  final FavoriteMovie favoriteMovies;
-  final FavoriteSerie favoriteSeries;
+  final List<PersonFavMovie> favoriteMovies;
+  final List<PersonFavSerie> favoriteSeries;
 
   Person({
     @required this.email,
@@ -23,12 +20,32 @@ class Person {
         email: json['email'] as String,
         name: json['name'] as String,
         photoURL: json['photoURL'] as String,
-        favoriteMovies: FavoriteMovie.fromJsonData(
-            json['movies'] as Map<String, dynamic> ?? {}),
-        favoriteSeries: FavoriteSerie.fromJsonData(
-            json['series'] as Map<String, dynamic> ?? {}),
+        favoriteMovies: List<PersonFavMovie>.from(json['movies'].map(
+          (x) => PersonFavMovie.fromJsonData(x as Map<String, dynamic>),
+        ) as Iterable),
+        favoriteSeries: List<PersonFavSerie>.from(json['series'].map(
+          (x) => PersonFavSerie.fromJsonData(x as Map<String, dynamic>),
+        ) as Iterable),
       );
 
   factory Person.fromFirebase(DocumentSnapshot doc) =>
       Person.fromJsonData(doc.data());
+}
+
+class PersonFavMovie {
+  final int favoriteMovieId;
+
+  PersonFavMovie({@required this.favoriteMovieId});
+
+  factory PersonFavMovie.fromJsonData(Map<String, dynamic> json) =>
+      PersonFavMovie(favoriteMovieId: json["id"] as int ?? -0);
+}
+
+class PersonFavSerie {
+  final int favoriteSerieId;
+
+  PersonFavSerie({@required this.favoriteSerieId});
+
+  factory PersonFavSerie.fromJsonData(Map<String, dynamic> json) =>
+      PersonFavSerie(favoriteSerieId: json["id"] as int ?? -0);
 }
