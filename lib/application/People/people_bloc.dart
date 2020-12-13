8759e6778580685a6c8dd9ae2db360695bfb91a2
Oluwatimilehin Diffu <thenifemi@gmail.com>
2020-12-12 21:39:47 -0300

    ..

--- lib/application/People/people_bloc.dart
+++ lib/application/People/people_bloc.dart
@@ -4,6 +4,7 @@ import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
+import 'package:movingPictures/domain/people/people_interface.dart';
 
 import '../../domain/people/people.dart';
 import '../../domain/people/people_failure.dart';
@@ -14,18 +15,29 @@ part 'people_state.dart';
 
 @injectable
 class PeopleBloc extends Bloc<PeopleEvent, PeopleState> {
-  PeopleBloc() : super(const _Initial());
+  final PeopleInterface peopleInterface;
+  StreamSubscription<Either<PeopleFailure, List<Person>>> peopleStream;
+
+  PeopleBloc(this.peopleInterface) : super(const _Initial());
 
   @override
   Stream<PeopleState> mapEventToState(
     PeopleEvent event,
   ) async* {
     yield* event.map(
-      followed: (e) async* {},
+      followed: (e) async* {
+        // yield const Peoples
+      },
       unfollowed: (e) async* {},
       watchPeople: (e) async* {},
       peopleRecieved: (e) async* {},
       personSearchQuery: (e) async* {},
     );
+  }
+
+  @override
+  Future<void> close() async {
+    await peopleStream?.cancel();
+    return super.close();
   }
 }
--- lib/application/People/people_state.dart
+++ lib/application/People/people_state.dart
@@ -3,6 +3,7 @@ part of 'people_bloc.dart';
 @freezed
 abstract class PeopleState with _$PeopleState {
   const factory PeopleState.initial() = _Initial;
+  const factory PeopleState.loading() = _Loading;
   const factory PeopleState.failure(PeopleFailure peopleFailure) = _Failure;
   const factory PeopleState.followSuccess() = _FollowSuccess;
   const factory PeopleState.unfollowSuccess() = _UnfollowSuccess;
